351f4247b4f9adf0be1d3b7478b309e6450fc516
Evgeny Zhurilin <zhurilin2014@yandex.ru>
2025-03-26 22:50:22 +0200

    Create and update project

--- SliderView/ViewController.swift
+++ SliderView/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        colorView.layer.masksToBounds = true
+        setupRedLabel()
+        setupGreenLabel()
+        setupBlueLabel()
+        updateColor()
     }
-
-
+    
+    @IBAction func sliderActionRed(){
+        redLabel.text = sliderRed.value.formatted()
+        updateColor()
+    }
+    @IBAction func sliderActionGreen(){
+        greenLabel.text = sliderGreen.value.formatted()
+        updateColor()
+    }
+    @IBAction func sliderActionBlue(){
+        blueLabel.text = sliderBlue.value.formatted()
+        updateColor()
+    }
+    
+    private func setupRedLabel() {
+        redLabel.text = sliderRed.value.formatted()
+    }
+    private func setupGreenLabel() {
+        greenLabel.text = sliderGreen.value.formatted()
+    }
+    private func setupBlueLabel() {
+        blueLabel.text = sliderBlue.value.formatted()
+    }
+    
+    func updateColor() {
+        let red = CGFloat(sliderRed.value)
+        let green = CGFloat(sliderGreen.value)
+        let blue = CGFloat(sliderBlue.value)
+        colorView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
